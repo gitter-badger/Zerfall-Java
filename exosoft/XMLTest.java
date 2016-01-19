2f29735af61daf2b818ca11169ada2e1781704a8
Campbell Jones <gingerdeadshot@gmail.com>
2016-01-18 22:21:09 -0500

    uh

--- exosoft/XMLTest.java
+++ exosoft/XMLTest.java
@@ -9,22 +9,38 @@ import org.xml.sax.SAXException;
 
 import com.piyush.STDMP;
 
-
-public class Test {
-
-    /**
-     * @param args
-     * @throws JAXBException 
-     * @throws SAXException 
-     */
-     
-    public static void main(String[] args) throws JAXBException, SAXException {
-        JAXBContext jaxbContext = JAXBContext.newInstance(STDMP.class);
-        SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-        STDMP ts = (STDMP)jaxbUnmarshaller.unmarshal(new File("resources/data/gun_data.xml"));
-
-        System.out.println(ts.getCoordSystems());
-        System.out.println(ts.getEDocument());
+public class XMLTest {
+    List<Object> parseXML(String xmlFile) {       
+        try {
+            DOMParser parser = new DOMParser();
+            parser.parse("mydocument.xml");
+            Document doc = parser.getDocument();
+ 
+            // Get the document's root XML node
+            NodeList root = doc.getChildNodes();
+ 
+            // Navigate down the hierarchy to get to the CEO node
+            Node comp = getNode("Company", root);
+            Node exec = getNode("Executive", comp.getChildNodes() );
+            String execType = getNodeAttr("type", exec);
+ 
+            // Load the executive's data from the XML
+            NodeList nodes = exec.getChildNodes();
+            String lastName = getNodeValue("LastName", nodes);
+            String firstName = getNodeValue("FirstName", nodes);
+            String street = getNodeValue("street", nodes);
+            String city = getNodeValue("city", nodes);
+            String state = getNodeValue("state", nodes);
+            String zip = getNodeValue("zip", nodes);
+ 
+            System.out.println("Executive Information:");
+            System.out.println("Type: " + execType);
+            System.out.println(lastName + ", " + firstName);
+            System.out.println(street);
+            System.out.println(city + ", " + state + " " + zip);
+        }
+        catch ( Exception e ) {
+            e.printStackTrace();
+        }
     }
 }
