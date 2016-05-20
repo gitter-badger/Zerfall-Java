1f2b384e7cb23f9b06b3732ae2302de5a7948259
Campbell Jones <gingerdeadshot@gmail.com>
2016-05-20 09:43:21 -0400

    Update Main.java

--- exosoft/zerfall/Main.java
+++ exosoft/zerfall/Main.java
@@ -40,58 +40,6 @@ public class Main extends Framework {
 		gameWorld.addObject(
 				new Object(new Point(50, 600), new Point(1080, 600), new Point(720, 625), new Point(250, 625)));
 		gameWorld.spawnEntity(player = new Character(SheetType.HORIZONTAL, "resources/sprites/player.png", 175, 161, keywatch), 0, 0);
-		metaHandler = new Timer(1000 / 120, new ActionListener() {
-			boolean pauseActionAvailable = true;
-			boolean consoleActionAvailable = true;
-
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				detectPauseAction();
-				detectConsoleAction();
-			}
-
-			private synchronized void detectConsoleAction() {
-				if (keywatch.getKey(KeyEvent.VK_BACK_QUOTE) && consoleActionAvailable) {
-					consoleInput.setVisible(!consoleInput.isVisible());
-					consoleInput.requestFocusInWindow();
-				}
-				consoleActionAvailable = !keywatch.getKey(KeyEvent.VK_BACK_QUOTE);
-			}
-
-			private synchronized void detectPauseAction() {
-				if (keywatch.getKey(KeyEvent.VK_ESCAPE) && pauseActionAvailable) {
-					if (gameHandler.isRunning()) {
-						gameHandler.stop();
-					} else {
-						gameHandler.start();
-					}
-				}
-				pauseActionAvailable = !keywatch.getKey(KeyEvent.VK_ESCAPE);
-			}
-		});
-
-		visualHandler = new Timer(1000 / drawRate, new ActionListener() {
-			long logTime = system.nanoTime();
-			
-			public void actionPerformed(ActionEvent e) {
-				calculateFramerate();
-				player.visual();
-				sheet.repaint();
-			}
-			
-			private void calculateFramerate(long currentTime) {
-				framerate = (System.nanoTime() - logTime).doubleValue() * 1000;
-				logTime = System.nanoTime();
-			} 
-		});
-
-		gameHandler = new Timer(1000 / gameFrequency, new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				player.movement();
-				player.physics();
-				gameWorld.execute();
-			}
-		});
 
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
